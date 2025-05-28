91389f239a7142bbffcd97afe5a79e303fb45d8a
Andres <ajtaboada04@gmail.com>
2025-05-28 18:15:56 +0200

    added bootstrap styles for homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,42 +1,49 @@
 class ArticlesController < ApplicationController
-
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
+  end
+
+  def show
   end
 
   def new
     @article = Article.new
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created."
+      flash[:notice] = 'Article was successfully created.'
       redirect_to @article
     else
-      render 'new'
+      render :new
     end
+  end
+
+  def edit
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated."
+      flash[:notice] = 'Article was successfully updated.'
       redirect_to @article
     else
-      render 'edit'
+      render :edit
     end
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_url
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
